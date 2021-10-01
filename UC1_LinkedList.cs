8f5fff296682ba28717af08707129393a3aecb5d
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-10-01 16:39:55 +0530

    UC1_simplelinkedList

---
+++ UC1_LinkedList.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LinkListAssignment
+{
+    class UC1_LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+    }
+}
